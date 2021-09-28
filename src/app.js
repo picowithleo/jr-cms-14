5443247972cb4518ad65528b77f57ef5fcd4e47c
Jinyuan Chen <picowithleo@gmail.com>
2021-09-28 22:02:58 +1000

    solve 502 problem

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const express = require('express');
 require('express-async-errors');
 const errorHandler = require('./middleware/errorHandler');
-const validationErrorHandler = require('./middleware/vaildationErrorHandler');
+const validationErrorHandler = require('./middleware/validationErrorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/', (req, res) => {
+  res.send('hello stranger');
+});
 app.use('/api', routes);
 
 app.use(validationErrorHandler);
